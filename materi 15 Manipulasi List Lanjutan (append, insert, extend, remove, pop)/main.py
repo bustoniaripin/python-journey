f715a83cbd5d909adf1991233c48d617bff9fe6d
bustoni arifin <bustoniaripin@gmail.com>
2026-09-02 21:08:16 +0700

    menambahkan materi 15

--- materi 15 Manipulasi List Lanjutan (append, insert, extend, remove, pop)/main.py
+++ materi 15 Manipulasi List Lanjutan (append, insert, extend, remove, pop)/main.py
@@ -2,3 +2,19 @@
 antrean = ["Budi","Siti"]
 
 # menambahkan file baru kebelakang
+antrean.append("Agus")
+
+# menyisipkan pelanggan prioritas kepaling depan (indeks 0)
+antrean.insert(0,"Bustoni")
+
+# menggabungkan rombongan baru ke antrean
+antrean.extend(["Alfina","Rumaisha"])
+
+# pelanggan batal antre berdasarkan nama
+antrean.remove("Budi")
+
+# melayani pelanggan paling depan dan mengambil namanya
+pelanggan_dilayani = antrean.pop(0)
+
+print(f"Pelanggan dilayani : {pelanggan_dilayani}")
+print(f"Sisa antrean : {antrean}")
